bef1e315357279e742f24414cd9f3b5aebdea59c
jennifer hodge <jenniferhodge@jennifers-Air.lan>
2023-11-07 18:59:13 -0600

    changed layout, do more tomorrow

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 
+let calculator = document.querySelector(".calculator");
+let screen = document.querySelector(".calculator-screen");
+let keys = document.querySelectorAll(".calculator-keys button");
+let clear = document.querySelector(".calculator-keys button.clear");
+let decimal = document.querySelector(".calculator-keys button.decimal");
+let equals = document.querySelector(".calculator-keys button.equals");
+let add = document.querySelector(".calculator-keys button.add");
+let subtract = document.querySelector(".calculator-keys button.subtract");
+let multiply = document.querySelector(".calculator-keys button.multiply");
+let divide = document.querySelector(".calculator-keys button.divide");
 
 
 
-const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.calculator__keys');
 
+let buttons = document.querySelectorAll(".calculator-keys button");
 
-keys.addEventListener('click', e => {
-    if(e.target.matches('button')){
-        
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        let input = this.innerHTML;
+        screen.innerHTML += input;
+    
+
     }
-})
+
+);}
